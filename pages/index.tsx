693ffe9118282bfffd36eb2d5baa54e36e52fa27
Ivanov Eduard <125977580+eduard134@users.noreply.github.com>
2023-07-21 12:57:39 +0300

    uh

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,11 +11,11 @@ export default function Home() {
           <meta name="description" content="" />
         </Head>
       </div>
-      <div>
-        <div>Favoarea mierei - artă a naturii</div>
-        <div>Mierea este un sărut al soarelui pentru pământ.</div>
+      <div className="sm:w-8/12 items-center m-auto mt-20">
+        <div className="text-7xl text-[#1c1e2b] font-bold">Mierea <br /> Proaspătă</div>
+        <div className="text-[#413f4a] py-3 font-medium ">Mierea crudă este un ingredient atât de simplu, <br /> conține polen și propolis</div>
         <div>
-          <button>Produse <span></span></button>
+          <button className="sm:border-2 bg-[#1c1e2b] text-white font-medium text-sm p-3 border-solid border-[#e6e5e2]-400 rounded-[50px]">Produse <span className="bg-[#d9b306] rounded-[50px] "> > </span></button>
         </div>
       </div>
     </>
