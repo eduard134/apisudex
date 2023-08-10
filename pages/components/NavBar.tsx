0acdaf620d5f3b5159085b5855f6bc255a308f2b
Loghin Dan <loghindan494@gmail.com>
2023-08-10 11:08:24 +0300

    update

--- pages/components/NavBar.tsx
+++ pages/components/NavBar.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     <div className="left-0 top-0 w-full z-10 mt-5">
       <div className="max-w-[1240px] m-auto sm:mt-4 sm:w-8/12 flex justify-between items-center sm:p-0 p-5 text-black  font-medium text-xl  sm:border-2 rounded-[50px] bg-white/70 drop-shadow-lg ">
         <Link href="/" className="flex items-center">
-          <Image src="/logo.png" alt="A&D" width={100} height={100} />
+          <Image src="/logo.png" alt="ApiSudex" width={100} height={100} />
           <p> ApiSudex</p>
         </Link>
         <ul className="hidden sm:flex">
