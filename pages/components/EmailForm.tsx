add05102852c4fa8db074aeb11b5bf507b198866
Loghin Dan <loghindan494@gmail.com>
2023-08-20 20:46:13 +0300

    upsate

--- pages/components/EmailForm.tsx
+++ pages/components/EmailForm.tsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import emailjs from 'emailjs-com';
+import React, { useState, useEffect } from "react";
+import emailjs from "emailjs-com";
 
 const EmailForm = () => {
-  const [sendername, setSenderName] = useState('');
-  const [to, setTo] = useState('');
-  const [message, setMessage] = useState('');
+  const [sendername, setSenderName] = useState("");
+  const [to, setTo] = useState("");
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     emailjs.init("I3DnisvYrfkPlxjpy");
@@ -33,48 +33,48 @@ const EmailForm = () => {
   return (
     <>
       <div className="px-12 flex-row w-full">
-              <p className="mb-2">Nume:</p>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                placeholder="Scrie acum..."
-                required
-                className="py-2 px-6 border-solid border-2 border-black rounded-md w-full"
-                value={sendername}
-                onChange={(e) => setSenderName(e.target.value)}
-              />
-              <p className="mb-2 mt-6">Email:</p>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Scrie adresa..."
-                required
-                className="py-2 px-6 border-solid border-2 border-black rounded-md w-full"
-                value={to}
-                onChange={(e) => setTo(e.target.value)}
-              />
-              <p className="mb-2 mt-6">Mesaj:</p>
-              <textarea
-                id="message"
-                name="message"
-                placeholder="Scrie un mesaj..."
-                required
-                className="py-2 px-6 border-solid border-2 border-black rounded-md w-full h-[15vh]"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-              />
-              <div>
-                <button
-                  type="submit"
-                  className="py-2 px-6 border-solid border-2 border-black rounded-md w-full mt-4 bg-gradient-to-br from-orange-500 to-yellow-500 transition-shadow-transform duration-150 ease-linear hover:shadow-lg hover:scale-105"
-                  onClick={sendMail}
-                >
-                  Trimite
-                </button>
-              </div>
-            </div>
+        <p className="mb-2">Nume:</p>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Scrie acum..."
+          required
+          className="py-2 px-6 border-solid border-2 border-black rounded-md w-full"
+          value={sendername}
+          onChange={(e) => setSenderName(e.target.value)}
+        />
+        <p className="mb-2 mt-6">Email:</p>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Scrie adresa..."
+          required
+          className="py-2 px-6 border-solid border-2 border-black rounded-md w-full"
+          value={to}
+          onChange={(e) => setTo(e.target.value)}
+        />
+        <p className="mb-2 mt-6">Mesaj:</p>
+        <textarea
+          id="message"
+          name="message"
+          placeholder="Scrie un mesaj..."
+          required
+          className="py-2 px-6 border-solid border-2 border-black rounded-md w-full h-[15vh]"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <div>
+          <button
+            type="submit"
+            className="py-2 px-6 border-solid border-2 border-black rounded-md w-full mt-4 bg-gradient-to-br from-orange-500 to-yellow-500 transition-shadow-transform duration-150 ease-linear hover:shadow-lg hover:bg-gradient-to-br hover:from-orange-600 hover:to-yellow-500"
+            onClick={sendMail}
+          >
+            Trimite
+          </button>
+        </div>
+      </div>
     </>
   );
 };
