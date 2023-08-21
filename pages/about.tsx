41588a05e81bf1d91842e8912af581404b5f70f4
Loghin Dan <loghindan494@gmail.com>
2023-08-21 16:07:56 +0300

    update

--- pages/about.tsx
+++ pages/about.tsx
@@ -20,17 +20,17 @@ const about = () => {
     <>
       <div className="flex flex-col items-center justify-center">
         <p className="text-center text-4xl font-recoleta">Despre Noi</p>
-        <p className="text-center text-md font-varela mt-6 leading-5">
+        <p className="text-center text-md font-varela mt-6 leading-5 lg:px-0 px-4">
           Ca Furnizor de Soluții pentru Apicultură, suntem dedicați creării de
           soluții personalizate pentru <br /> creșterea și dezvoltarea
           afacerilor apicole prospere.
         </p>
-        <div className="w-[80%] flex justify-center items-center mt-10">
+        <div className="w-[80%] lg:flex justify-center items-center mt-10 block">
           <div className="mb-4 h-[80%]  ">
             <Image src={beeing} alt="" className="rounded-xl" />
           </div>
           <div className="px-8">
-            <div className="font-varela w-[70%]">
+            <div className="font-varela lg:w-[70%] w-[full] lg:text-start text-center">
               <p className="mb-4">
                 Experiența noastră solidă de 14 ani în apicultură ne motivează
                 să vă prezentăm cu pasiune compania noastră, ApiSudex,
@@ -52,20 +52,22 @@ const about = () => {
                 astfel importanța unică a fiecărui element din acest ecosistem.
               </p>
             </div>
-            <button
-              onClick={handleContactClick}
-              className="font-varela hover:bg-gradient-to-br hover:from-yellow-500 hover:via-orange-500 hover:to-yellow-500 text-black border-2 border-orange-500 rounded-lg transition duration-300 hover:shadow-md hover:text-white mt-6"
-            >
-              <p className="px-5 py-3">Contactați-ne</p>
-            </button>
+            <div className="flex justify-center lg:block">
+              <button
+                onClick={handleContactClick}
+                className="font-varela hover:bg-gradient-to-br hover:from-yellow-500 hover:via-orange-500 hover:to-yellow-500 text-black border-2 border-orange-500 rounded-lg transition duration-300 hover:shadow-md hover:text-white mt-6"
+              >
+                <p className="px-5 py-3">Contactați-ne</p>
+              </button>
+            </div>
           </div>
         </div>
         <p className="text-center text-4xl font-recoleta mt-14">
           De ce să ne alegeți
         </p>
-        <div className="flex flex-wrap justify-center font-varela mt-10">
-          <div className="bg-[#F9E9B8] w-[34%] h-[full] m-2 rounded-xl p-4">
-            <div className="w-[3%] absolute -mt-12 bg-yellow-400 p-3 rounded-xl ml-2">
+        <div className="lg:flex lg:flex-wrap block justify-center font-varela mt-10">
+          <div className="bg-[#F9E9B8] lg:w-[34%] h-[full] m-2 mt-10 rounded-xl p-4">
+            <div className="lg:w-[3%] w-[17.5%] sm:w-[7.5%] absolute -mt-12 bg-yellow-400 p-3 rounded-xl ml-2">
               <Image src={quality} alt="" />
             </div>
             <div className="mt-4 text-lg font-semibold">
@@ -77,8 +79,8 @@ const about = () => {
               adaptate pentru a vă ajuta să atingeți succesul în apicultură.
             </div>
           </div>
-          <div className="bg-[#F9E9B8] w-[34%] h-[full] m-2 rounded-xl p-4">
-            <div className="w-[3%] absolute -mt-12 bg-yellow-400 p-3 rounded-xl ml-2">
+          <div className="bg-[#F9E9B8] lg:w-[34%] h-[full] m-2 mt-10 rounded-xl p-4">
+            <div className="lg:w-[3%] w-[17.5%] sm:w-[7.5%] absolute -mt-12 bg-yellow-400 p-3 rounded-xl ml-2">
               <Image src={high_quality} alt="" />
             </div>
             <div className="mt-4 text-lg font-semibold">
@@ -91,8 +93,8 @@ const about = () => {
               detalii și din materiale durabile.{" "}
             </div>
           </div>
-          <div className="bg-[#F9E9B8] w-[34%] h-[full] mx-2 mt-10 rounded-xl p-4">
-            <div className="w-[3%] absolute -mt-12 bg-yellow-400 p-3 rounded-xl ml-2">
+          <div className="bg-[#F9E9B8] lg:w-[34%] h-[full] mx-2 mt-10 rounded-xl p-4">
+            <div className="lg:w-[3%] w-[17.5%] sm:w-[7.5%] absolute -mt-12 bg-yellow-400 p-3 rounded-xl ml-2">
               <Image src={innovation} alt="" />
             </div>
             <div className="mt-4 text-lg font-semibold">
@@ -105,8 +107,8 @@ const about = () => {
               inovatoare pentru monitorizarea stupilor și colectarea mierii.
             </div>
           </div>
-          <div className="bg-[#F9E9B8] w-[34%] h-[full] mx-2 mt-10 rounded-xl p-4">
-            <div className="w-[3%] absolute -mt-12 bg-yellow-400 p-3 rounded-xl ml-2">
+          <div className="bg-[#F9E9B8] lg:w-[34%] h-[full] mx-2 mt-10 rounded-xl p-4">
+            <div className="lg:w-[3%] w-[17.5%] sm:w-[7.5%] absolute -mt-12 bg-yellow-400 p-3 rounded-xl ml-2">
               <Image src={relation} alt="" />
             </div>
             <div className="mt-4 text-lg font-semibold">
